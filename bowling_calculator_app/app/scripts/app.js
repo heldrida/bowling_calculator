2a2df5b78da4143e78b151dbaba760e434dbcd79
heldrida <heldrida@gmail.com>
2014-07-10 23:40:12 +0000

    deleted unwanted controller and view file. created basic html structure for bowling scores. added some styling.

--- bowling_calculator_app/app/scripts/app.js
+++ bowling_calculator_app/app/scripts/app.js
@@ -1,4 +1,22 @@
 'use strict';
 
-angular
-  .module('bowlingCalculatorAppApp', []);
+angular.module('bowlingCalculatorAppApp', [])
+
+.controller('MainCtrl', function ($scope) {
+
+	var frameModel = {
+		firstAtt: 0,
+		secondAtt: 0,
+		score: 0
+	};
+
+	$scope.frames = [];
+	$scope.total = 0;
+	$scope.user = {};
+	$scope.user.activeIndex = 2;
+
+	for (var i = 0; i < 10; i++){
+		$scope.frames.push(frameModel);
+	};
+
+});
