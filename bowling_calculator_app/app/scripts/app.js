abdd693e5db83eba446ae5976b7c9cd5699e45a0
heldrida <heldrida@gmail.com>
2014-07-11 00:32:44 +0000

    created setScore method

--- bowling_calculator_app/app/scripts/app.js
+++ bowling_calculator_app/app/scripts/app.js
@@ -4,11 +4,11 @@ angular.module('bowlingCalculatorAppApp', [])
 
 .controller('MainCtrl', function ($scope) {
 
-	var frameModel = {
+	var frameModel = [{
 		firstAtt: 0,
 		secondAtt: 0,
 		score: 0
-	};
+	}];
 
 	$scope.frames = [];
 	$scope.total = 0;
@@ -16,8 +16,10 @@ angular.module('bowlingCalculatorAppApp', [])
 	$scope.user.activeIndex = 0;
 
 	for (var i = 0; i < 10; i++){
-		$scope.frames.push(frameModel);
+		$scope.frames.push(frameModel.slice(0));
 	};
+
+	console.log($scope.frames);
 
 	$scope.nextFrame = function(){
 		
@@ -41,8 +43,28 @@ angular.module('bowlingCalculatorAppApp', [])
 	$scope.shotNumbers = [];
 
 	for (var i = 1; i < 11; i++){
-		
+
 		$scope.shotNumbers.push(i);
+
+	};
+
+
+	var lock = false;
+	$scope.setScore = function(obj, $event){
+
+		var el = $event.target;
+		var score = angular.element(el).attr('score');
+
+		if (!lock){
+		
+			$scope.frames[$scope.user.activeIndex].firstAtt = score;
+			lock = true;
+		} else {
+
+			$scope.frames[$scope.user.activeIndex].secondAtt = score;
+			lock = false;
+			$scope.user.activeIndex += 1;
+		}
 
 	};
 
