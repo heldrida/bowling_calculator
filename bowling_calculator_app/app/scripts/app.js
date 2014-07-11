fcea6d68bebbfadb219160f7013007741effb2d6
heldrida <heldrida@gmail.com>
2014-07-11 00:04:18 +0000

    created new controller. new html elements improving interface, so  user can pick score values

--- bowling_calculator_app/app/scripts/app.js
+++ bowling_calculator_app/app/scripts/app.js
@@ -13,10 +13,37 @@ angular.module('bowlingCalculatorAppApp', [])
 	$scope.frames = [];
 	$scope.total = 0;
 	$scope.user = {};
-	$scope.user.activeIndex = 2;
+	$scope.user.activeIndex = 0;
 
 	for (var i = 0; i < 10; i++){
 		$scope.frames.push(frameModel);
+	};
+
+	$scope.nextFrame = function(){
+		
+		if ($scope.user.activeIndex < $scope.frames.length - 1){
+	
+			$scope.user.activeIndex += 1;
+
+		} else if ($scope.user.activeIndex === $scope.frames.length - 1) {
+
+			$scope.user.activeIndex = 0;
+
+		}
+
+	};
+
+})
+
+.controller('frameCtrl', function ($scope) {
+
+
+	$scope.shotNumbers = [];
+
+	for (var i = 1; i < 11; i++){
+		
+		$scope.shotNumbers.push(i);
+
 	};
 
 });
